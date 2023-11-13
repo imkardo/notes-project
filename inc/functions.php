9d7b228b62145bb9a85114628ff73a02db9a9618
Kardo Heydari <kardoheydari.1387@gmail.com>
2023-11-13 16:40:53 +0330

    Update functions.php

--- inc/functions.php
+++ inc/functions.php
@@ -1,9 +1,10 @@
 <?php 
+require_once 'db.php';
 if(isset($_POST['do-register'])){
 $displayname = $_POST['display-name'];
 $username = $_POST['username'];
 $password = $_POST['password'];
 $passwordConf = $_POST['password-conf'];
 
-echo $displayname . $username . $password . $passwordConf;
+mysqli_query($db, "INSERT INTO users(display_name, username, password) VALUES ('$displayname','$username','$password')");
 }
